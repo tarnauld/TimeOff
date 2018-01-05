ff482aaf5d4bdd66073581da4bd22c281ee79bd9
uK4 <timothee.arnauld1@gmail.com>
2018-01-05 21:08:11 +0100

    New file

---
+++ TimeOffEvents/Commands.fs
@@ -0,0 +1,30 @@
+module TimeOff.Commands
+
+open System
+open JsonConvert
+open EventStorage
+
+let store = InMemoryStore.Create<UserId, RequestEvent>()
+
+let executeAllCommands (events: RequestEvent list, command) = 
+  for event in events do
+    let stream = store.GetStream event.Request.UserId
+    stream.Append [event]
+    Logic.handleCommand store command |> 
+    ignore
+
+let reverse command events = events, command
+  
+let addHoliday uid leftBound rightBound =
+  let request = {
+        UserId = uid
+        RequestId = Guid.Empty
+        Start = leftBound
+        End = rightBound }
+
+  let result = [] |> reverse ( RequestTimeOff request ) |> executeAllCommands
+  JsonConvert.JSON result
+
+let getHoliday =
+  let result = Logic.getRequestState [] 
+  JsonConvert.JSON result
--- TimeOffEvents/ConfigRoutes.fs
+++ TimeOffEvents/ConfigRoutes.fs
@@ -3,32 +3,8 @@ module TimeOff.Routes
 open Suave
 open Suave.Filters
 open Suave.Operators
-open EventStorage
-open JsonConvert
+open Commands
 open System
-let store = InMemoryStore.Create<UserId, RequestEvent>()
-
-let executeAllCommands (events: RequestEvent list, command) = 
-  for event in events do
-    let stream = store.GetStream event.Request.UserId
-    stream.Append [event]
-  Logic.handleCommand store command
-
-let reverse command events = events, command
-  
-let addHoliday uid leftBound rightBound =
-  let request = {
-        UserId = uid
-        RequestId = Guid.Empty
-        Start = leftBound
-        End = rightBound }
-
-  let result = [] |> reverse ( RequestTimeOff request ) |> executeAllCommands
-  JsonConvert.JSON result
-
-let getHoliday =
-  let result = Logic.getRequestState [] 
-  JsonConvert.JSON result
 
 let app = 
     choose
